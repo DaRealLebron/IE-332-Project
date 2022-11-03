95193d21ab1da55bbdb0b37039616bfc05438a45
DaRealLebron <92001129+DaRealLebron@users.noreply.github.com>
2022-11-03 14:41:44 -0400

    Update A4-1.R

--- A4-1.R
+++ A4-1.R
@@ -4,3 +4,7 @@ df_raw<- read.csv("C:/Users/12026/Desktop/detailed_finances_dataset.csv",header=
 df_raw$Sector<- as.factor(df_raw$Sector)
 
 #b
+goodCol<-!sapply(1:length(df_raw), function(i){sum(is.na(df_raw[,i]))>1000})
+df<-na.omit(df_raw[,goodCol])
+
+#c
